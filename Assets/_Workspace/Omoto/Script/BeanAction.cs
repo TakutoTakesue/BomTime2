fe6178aa19018b2da55ac1429759cc6f1fc0d193
miyu0704 <omoto.m.0724@gmail.com>
2021-12-15 12:59:15 +0900

    item_20211215

---
+++ Assets/_Workspace/Omoto/Script/BeanAction.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*フィールドに生成されるアイテム　拾われたとき*/
+
+public class BeanAction : MonoBehaviour
+{ 
+    GameObject Gun;
+
+    public float DestoryCnt=10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Gun = GameObject.FindGameObjectWithTag("Gun");
+
+        // 60秒経過で削除　(外してもいいかなぁ)
+        Destroy(gameObject, DestoryCnt);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // プレイヤーと触れたとき　(ゲットされたら)
+        if (other.gameObject.tag == "Player")
+        {
+            // 銃の弾の数を増やす
+            Gun.SendMessage("Bullet_Add",SendMessageOptions.DontRequireReceiver);
+
+            // 自分は消える
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/_Workspace/Omoto/Script/Gun.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float Vel = 45.0f; //弾の初速度
+    public float interval = 0.5f; //弾の射出間隔
+
+    int Bullet_cnt; // 弾の残り数
+
+    public GameObject Bean_Bullet; //発射用弾
+    GameObject B;   // 発射用を生成
+   
+    bool isTrigger = false; //　銃が発射されているか,打てるのか
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 銃の発射待機
+        StartCoroutine("Shot");
+
+        Bullet_cnt = 5;
+    }
+
+    IEnumerator Shot()
+    {
+        // 打たれる待機
+        while (true)
+        {
+
+            Vector3 Bullet_pos;
+            Bullet_pos.y = transform.position.y + 0.5f;
+            Bullet_pos.x = transform.position.x;
+            Bullet_pos.z = transform.position.z + 1;
+
+            if (isTrigger)
+            { 
+                B = Instantiate(Bean_Bullet,Bullet_pos , Quaternion.identity);
+                B.GetComponent<Rigidbody>().velocity = transform.position * Vel;
+
+                Bullet_cnt--;
+            }
+            yield return new WaitForSeconds(interval); //射出間隔だけ待つ
+        }
+    }
+
+    // 装弾数の加算
+    void Bullet_Add()
+    {
+        Bullet_cnt++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Bullet_cntが0より大きければトリガーを引ける.
+        if (Bullet_cnt > 0)
+        {
+            isTrigger = Input.GetKey(KeyCode.Space);
+        }
+        else
+        {
+            isTrigger = false;
+        }
+    }
+}
