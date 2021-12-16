e7ccb37c025090f29d1778dc341d0bd7bf846f8e
miyu0704 <omoto.m.0724@gmail.com>
2021-12-16 16:10:50 +0900

    itemUpdate_12.16

---
+++ Assets/_Workspace/Omoto/Script/Bullet_Action.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_Action : MonoBehaviour
+{
+    public GameObject HitEffect; //衝突時のエフェクト
+
+   
+    // 消えるまでの時間
+    public float DestroyCnt = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 発射されてから自身を削除
+        Destroy(gameObject, DestroyCnt);
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        //何かに当たったらエフェクトを表示して消える.
+        Instantiate(HitEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/_Workspace/Omoto/Script/HealItem.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealItem : MonoBehaviour
+{
+    GameObject Player;
+
+    // HP回復量
+    public float recovery_amount = 50.0f;
+
+    public float DestoryCnt = 30.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+
+        transform.rotation= Quaternion.AngleAxis(-90.0f, new Vector3(1, 0, 0));
+
+        // 60秒経過で削除　(外してもいいかなぁ)
+        Destroy(gameObject, DestoryCnt);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        // プレイヤーと触れたとき　(ゲットされたら)
+        if (other.gameObject.tag == "Player")
+        {
+            // プレイヤーのHPを回復
+            /* Player.SendMessage("HP_Heal",recovery_amount,SendMessageOptions.DontRequireReceiver);
+             *
+             * もしくは
+             * 
+             * Player.HP+=recovery_amount 
+             * 
+             * など未定
+             */
+
+
+            // 自分は消える
+            Destroy(gameObject);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 回転
+        transform.Rotate(new Vector3(0.0f, 0.0f, 0.3f));
+        // 上下
+        transform.position = new Vector3(transform.position.x, Mathf.Sin(Time.time) * 0.45f, transform.position.z);
+    }
+}
