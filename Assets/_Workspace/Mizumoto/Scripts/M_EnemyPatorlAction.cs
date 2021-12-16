3314dc3e0cd7b100d7b8604725ca7bbab0a80eaa
miyu0704 <omoto.m.0724@gmail.com>
2021-12-16 16:07:08 +0900

    Merge branch 'main' of https://github.com/TakutoTakesue/BomTime2

---
+++ Assets/_Workspace/Mizumoto/Scripts/M_EnemyPatorlAction.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class M_EnemyPatorlAction : MonoBehaviour
+{
+    [SerializeField, Header("敵の徘徊ルートの分別")]
+    int patrolNo = 0;
+    // 受け取り用
+    public int PatrolNo { get {return patrolNo;}}
+    [SerializeField, Header("敵の徘徊ルートの順番")]
+    int patrolOrder = 0; // 0番目 1番目 2番目の順番で動いたりして次の番号がなくなった場合0に戻る
+    public int PatrolOrder { get { return patrolOrder; } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
