78f47364d6603a4861e7c56ced9fd9e1327d630d
miyu0704 <omoto.m.0724@gmail.com>
2021-12-17 16:37:07 +0900

    a
    a

---
+++ Assets/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            
+        }
+    }
+}
