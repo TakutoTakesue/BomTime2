24ad9d3893fff519d728b78bf06fb04fe90da457
miyu0704 <omoto.m.0724@gmail.com>
2022-01-14 16:43:55 +0900

    timeUI

--- Assets/_Workspace/Omoto/Script/GameManager.cs
+++ Assets/_Workspace/Omoto/Script/GameManager.cs
@@ -6,17 +6,19 @@ public class GameManager : MonoBehaviour
 {
     Play_UI_Managet P_UI_MGR;
 
-    float Elapsed = 0.5f;
+    float Elapsed = 60.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        P_UI_MGR = GetComponent<Play_UI_Managet>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Elapsed -= Time.deltaTime;
+
         P_UI_MGR.Time_UI(Elapsed);
     }
 }
