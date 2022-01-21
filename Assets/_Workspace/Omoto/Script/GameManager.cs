279af5c84215a95c25d4e771983ec8650f7e3062
miyu0704 <omoto.m.0724@gmail.com>
2022-01-21 18:01:21 +0900

    a

--- Assets/_Workspace/Omoto/Script/GameManager.cs
+++ Assets/_Workspace/Omoto/Script/GameManager.cs
@@ -5,16 +5,11 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
 
-    float Elapsed = 100.0f;
-
-    GameObject UI_MGR;
-    Play_UI_Managet P_UI_MGR;
+   public float Elapsed = 100.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        UI_MGR = GameObject.FindGameObjectWithTag("GameController");
-        P_UI_MGR = UI_MGR.GetComponent<Play_UI_Managet>();
 
     }
 
@@ -22,11 +17,5 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         Elapsed -= Time.deltaTime;
-
-        P_UI_MGR.Time_UI(Elapsed);
-
-        /*UIテスト用*/
-        P_UI_MGR.HealItem_UI(1);
-        P_UI_MGR.PlayerHP_Bar(90, 100);
     }
 }
--- Assets/_Workspace/Omoto/Script/Play_UI_Managet.cs
+++ Assets/_Workspace/Omoto/Script/Play_UI_Managet.cs
@@ -20,39 +20,56 @@ public class Play_UI_Managet : MonoBehaviour
 
     [SerializeField] Slider slider;
 
+    [SerializeField] private PlayerAction PlayerScript;
+    [SerializeField] private GameManager ManaerScript;
+    [SerializeField] private Gun GunScript;
+
+    int Bullet_cnt;
+    float TIME;
+    int ItemCnt;
+    int PlayerHP;
+    int MaxHP;
+
     // Start is called before the first frame update
     void Start()
     {
         Init();
-        Time_UI(0.0f);
-
+        Time_UI();
         HealImage.SetActive(false);
-
         //Sliderを満タンにする。
         slider.value = 1;
+
+        GameObject player = GameObject.FindWithTag("Player");
+        PlayerScript = player.GetComponent<PlayerAction>();
+
+        GameObject gamemanager = GameObject.FindWithTag("GameController");
+        ManaerScript = gamemanager.GetComponent<GameManager>();
+
+        GameObject gun = GameObject.FindWithTag("Gun");
+        GunScript = gun.GetComponent<Gun>();
+
     }
 
     // 初期化
     public void Init()
     {
-        txtBullet_cnt.text = "";
-        txtTime.text = "Time:"+"88.888";
+
     }
 
     // 弾の残り弾数表示
-    public void Bullet_UI(int Bullet_cnt)
+    public void Bullet_UI()
     {
         txtBullet_cnt.text = ":"+ Bullet_cnt.ToString().PadLeft(3, '0');
     }
 
     // 残り時間表示
-    public void Time_UI(float TIME)
+    public void Time_UI()
     {
         txtTime.text = "TIME:" + TIME.ToString("f0").PadLeft(3, '0');
     }
 
     //恵方巻の表示非表示 所持数0以下なら非表示
-    public void HealItem_UI(int ItemCnt)
+    public void HealItem_UI()
     {
         if (ItemCnt <= 0) 
         {
@@ -65,14 +82,19 @@ public class Play_UI_Managet : MonoBehaviour
     }
 
     //プレイヤーのHPバーの増減
-    public void PlayerHP_Bar(int PlayerHP,int MaxHP)
+    public void PlayerHP_Bar()
     {
         slider.value = (float)PlayerHP / (float)MaxHP;
     }
 
+   
     // Update is called once per frame
     void Update()
     {
-        
+        TIME = ManaerScript.Elapsed;
+
+
+
+        Time_UI();
     }
 }
