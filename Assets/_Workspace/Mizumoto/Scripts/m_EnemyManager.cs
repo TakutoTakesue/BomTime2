ba5a5ff3230a5eff875374e02017906012b6bc1b
mizumo3 <92150582+mizumo3@users.noreply.github.com>
2022-01-21 17:36:07 +0900

    a

---
+++ Assets/_Workspace/Mizumoto/Scripts/m_EnemyManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class m_EnemyManager : MonoBehaviour
+{
+    int enemyCount = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var enemys = GameObject.FindGameObjectsWithTag("Enemy");
+        foreach(var enemy in enemys){
+            ++enemyCount;
+        }
+        if (enemyCount == 0) {
+            // ゲームマネージャーにゲームを終了する処理を送る
+        }
+    }
+
+    public void EnemyDead() {
+        --enemyCount;
+        if (enemyCount <= 0) {
+            // ゲームマネージャーにゲームを終了する処理を送る
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
