985a373fac1032c46a7f54265f7576b4d4fe7196
FujitoKai <88478585+FujitoKai@users.noreply.github.com>
2022-01-21 16:52:15 +0900

    a

--- Assets/_Workspace/Fujito/Script/BulletAction.cs
+++ Assets/_Workspace/Fujito/Script/BulletAction.cs
@@ -5,15 +5,30 @@ using UnityEngine;
 public class BulletAction : MonoBehaviour
 {
     Rigidbody myRb;
+    [SerializeField] int power;
     [SerializeField] float addPower;
     [SerializeField, Tooltip("発射後に消えるまでの秒数")] float destroySeconds;
 
+    public int GetPower
+    {
+        get { return power; }
+    }
+
     // Start is called before the first frame update
+
     void Start()
     {
         myRb = GetComponent<Rigidbody>();
         myRb.AddForce(transform.forward * addPower);
         Destroy(gameObject, destroySeconds);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag != "Enemy")
+        {
+            Destroy(gameObject);
+        }
     }
 
     // Update is called once per frame
