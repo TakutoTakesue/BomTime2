dc1f47884b9edd02d2c2a06207156bb36f5d0f97
miyu0704 <omoto.m.0724@gmail.com>
2022-02-04 17:07:18 +0900

    a

--- Assets/_Workspace/Omoto/Script/GameManager.cs
+++ Assets/_Workspace/Omoto/Script/GameManager.cs
@@ -1,21 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    // 経過時間.
+    public float Elapsed = 0.0f;
 
-   public float Elapsed = 100.0f;
+    //  シーン遷移.
+    enum Scene
+    {
+        Title,
+        Select,
+        Play,
+        Ranking
+    }
+
+    // ステージ数.
+    enum StageNum
+    {
+        stage1=0
+    }
+
+    Scene scene;
+    StageNum stage;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        scene = Scene.Title;
+        stage = StageNum.stage1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Elapsed -= Time.deltaTime;
+ 
+        switch(scene)
+        {
+            case Scene.Title:
+
+                //if()
+                //{
+                //    Elapsed = 0.0f;
+                //}
+
+                break;
+
+            case Scene.Play:
+                Elapsed += Time.deltaTime;
+                break;
+        }
+
     }
 }
