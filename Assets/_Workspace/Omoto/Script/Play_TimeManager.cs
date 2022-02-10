9faa5bea7b8aea571068edb3296944015e8054b1
miyu0704 <omoto.m.0724@gmail.com>
2022-02-11 03:40:09 +0900

    aaaa

--- Assets/_Workspace/Omoto/Script/Play_TimeManager.cs
+++ Assets/_Workspace/Omoto/Script/Play_TimeManager.cs
@@ -9,33 +9,55 @@ public class Play_TimeManager : MonoBehaviour
     public float Elapsed = 0.0f;
 
     public static float Score;
- 
-    // ランキング用
-    float[] Rank = new float[6];
+
+    bool GameOverFlg = false;
+
+    //プレイヤーのオブジェクト
+    [SerializeField] GameObject player;
+    M_StateAction act_MState;
+    PlayerAction act_Player;
 
     // Start is called before the first frame update
     void Start()
     {
-        AudioManager.Instance.StartBgm("Play_BGM", 2.0f);   
+        AudioManager.Instance.StartBgm("Play_BGM", 2.0f);
+
+        act_MState = player.GetComponent<M_StateAction>();
+        act_Player = player.GetComponent<PlayerAction>();
+
+        Score = 0.0f;
     }
 
     public void GameEnd()
     {
-        Score = Elapsed;
-        
         Invoke("NextScene",2.0f);
 
     }
 
     void NextScene()
     {
-        SceneManager.LoadScene("Rank");
+        if(GameOverFlg)
+        {
+            SceneManager.LoadScene("Select");
+        }
+        else 
+        {
+            Score = Elapsed;
+            SceneManager.LoadScene("Rank");
+        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         Elapsed += Time.deltaTime;
+
+        if(act_MState.HP<=0)
+        {
+            GameOverFlg = true;
+            GameEnd();
+        }
         
     }
 }
