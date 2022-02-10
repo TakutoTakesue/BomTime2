2d8993c2933ed8935c0e6577357f6d401f5dbc25
miyu0704 <omoto.m.0724@gmail.com>
2022-02-11 07:49:31 +0900

    a

---
+++ Assets/_Workspace/Omoto/Script/Title&Select/Title_playerobject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Title_playerobject : MonoBehaviour
+{ 
+    Animator myAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
